3281b5b8c395ed167ae37d06d75f7b9adda8485d
10200019 <bilinoor39@gmail.com>
2021-10-03 22:16:55 +0700

    wp

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"nama saya Sabili Noor saya tinggal di daerah jakarta olah raga yang
+            saya suka adalah bulutangkis";
+    }
+}
